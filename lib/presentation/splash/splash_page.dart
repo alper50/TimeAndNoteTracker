85806a4e4cc7a4ca55b1f225c948be81bf1282f9
alper50 <alperyn50@gmail.com>
2022-04-22 18:26:55 +0300

    firebase and google sign setup

--- lib/presentation/splash/splash_page.dart
+++ lib/presentation/splash/splash_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({ Key? key }) : super(key: key);
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
 
+class _SplashPageState extends State<SplashPage> {
+  
+  @override
+  void initState()async{
+    super.initState();
+    await Firebase.initializeApp();  
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
